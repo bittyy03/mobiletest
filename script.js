c2fd15b53465d2d9ab156239a8b48365c27fdeeb
bittyy03 <89165229+bittyy03@users.noreply.github.com>
2025-02-17 01:29:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.addEventListener('load', function() {
+    // 1-2초 후 로딩 화면 숨기기
+    setTimeout(function() {
+        document.getElementById('loadingScreen').style.display = 'none';
+        document.getElementById('content').style.display = 'block'; // 실제 콘텐츠 보이기
+    }, 2000); // 2초 후에 로딩 화면을 숨기고 실제 콘텐츠를 보여줍니다.
+});
+
 // 알림 배너와 버튼을 찾습니다.
 const notificationBanner = document.getElementById('notificationBanner');
 const closeNotificationBtn = document.getElementById('closeNotificationBtn');
